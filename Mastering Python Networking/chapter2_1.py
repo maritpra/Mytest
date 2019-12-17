85e427a8ed93ff286001fd43bbf7c40085e5c2c3
maritpra <maritpra@penguin.lxd>
2019-12-17 11:04:52 +0700

    updated

--- Mastering Python Networking/chapter2_1.py
+++ Mastering Python Networking/chapter2_1.py
@@ -13,4 +13,11 @@ for device in devices.keys():
     device_prompt = devices[device['prompt']]
     child = pexpect.spawn('telnet ' + devices[device]['ip'])
     child.expect('Username')
-    child.sendline('username')
+    child.sendline(username)
+    child.expect('Password')
+    child.sendline(password)
+    child.expect(device_prompt)
+    child.sendline('show ver | i V')
+    child.expect(device_prompt)
+    print(child.before)
+    child.sendline('exit')
