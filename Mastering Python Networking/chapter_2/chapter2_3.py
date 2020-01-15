fde837e8683477c7b06ea5d656de9a87e09048b3
maritpra <maritpra@penguin.lxd>
2020-01-15 15:29:17 +0700

    update

--- Mastering Python Networking/chapter_2/chapter2_3.py
+++ Mastering Python Networking/chapter_2/chapter2_3.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
 
-import pexpect
+import paramiko
 import getpass
+import time
 
 devices = {
-    'R1': {'prompt': 'R1#', 'ip': '192.168.122.14'},
-    'R2': {'prompt': 'R2#', 'ip': '192.168.122.25'}
+    'R1': {'ip': '192.168.122.14'},
+    'R4': {'ip': '192.168.122.252'}
     }
-commands = ['term len 0', 'show version', 'show run']
+commands = ['show version\n', 'show run\n']
+
+max_buffer = 10000
+def clear_buffer(connection):
+    if connection.recv_ready():
+        return connection.recv(max_buffer)
 
 username = input('Username: ')
 password = getpass.getpass('Password: ')
 
 for device in devices.keys():
     outputFileName = device + '_output.txt'
-    device_prompt = devices[device]['prompt']
-    child = pexpect.spawn('telnet ' + devices[device]['ip'])
-    child.expect('Username')
-    child.sendline(username)
-    child.expect('Password')
-    child.sendline(password)
-    child.expect(device_prompt)
+    connection = paramiko.SSHClient()
+    connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    connection.connect(devices[device]['ip'], username=username, password=password, look_for_keys=False, allow_agent=False)
+    new_connection = connection.invoke_shell()
+    output = clear_buffer(new_connection)
+    time.sleep(2)
+    new_connection.send("term len 0\n")
+    output = clear_buffer(new_connection)
     with open(outputFileName, 'wb') as f:
         for command in commands:
-            child.sendline(command)
-            child.expect(device_prompt)
-            f.write(child.before)
-        child.sendline('exit')
+            new_connection.send(command)
+            time.sleep(2)
+            output = new_connection.recv(max_buffer)
+            print(output)
+            f.write(output)
+    new_connection.close()
