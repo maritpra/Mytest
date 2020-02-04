80f8aae8ad29e298243f88cfc234588622785dd6
maritpra <maritpra@hotmail.com>
2020-02-04 17:14:51 +0700

    update

--- Mastering Python Networking/chapter_3/chapter3_2 aci.py
+++ Mastering Python Networking/chapter_3/chapter3_2 aci.py
@@ -3,26 +3,30 @@
 import requests
 import json
 
-url='http://sbx-nxos-mgmt.cisco.com/ins'
-switchuser='admin'
-switchpassword='Admin_1234!'
+controller='https://sandboxapicdc.cisco.com'
+user='admin'
+password='ciscopsdt'
 
-myheaders={'content-type':'application/json-rpc'}
-payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "show version",
-      "version": 1
-    },
-    "id": 1
-  }
-]
-response = requests.post(
-    url,
-    data=json.dumps(payload), 
-    headers=myheaders,
-    auth=(switchuser,switchpassword)
-).json()
-print(response['result']['body']['sys_ver_str'])
+def gettoken(apic_ip, user, password):
+    url = apic_ip + "/api/aaaLogin.json"
+    payload = {
+      "aaaUser":{
+        "attributes":{
+          "name":user,
+          "pwd":password
+        }
+      }
+    }
+    header = {'content-type':'application/json'}
+    response = requests.post(
+      url,
+      data= json.dumps(payload),
+      headers= header,
+      verify= False
+    )
+    r_json = response.json()
+    token = r_json['imdata'][0]['aaaLogin']['attributes']['token']
+    return token
+
+thetoken = gettoken(controller, user, password)
+#print(thetoken)
