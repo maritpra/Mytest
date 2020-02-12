297ab25c225855954060c174949959b18c53022e
maritpra <maritpra@penguin.lxd>
2020-02-12 14:06:30 +0700

    update

--- Mastering Python Networking/chapter_3/chapter3_2 aci.py
+++ Mastering Python Networking/chapter_3/chapter3_2 aci.py
@@ -8,25 +8,36 @@ user='admin'
 password='ciscopsdt'
 
 def gettoken(apic_ip, user, password):
-    url = apic_ip + "/api/aaaLogin.json"
-    payload = {
-      "aaaUser":{
-        "attributes":{
-          "name":user,
-          "pwd":password
-        }
+  url = apic_ip + "/api/aaaLogin.json"
+  payload = {
+    "aaaUser":{
+      "attributes":{
+        "name":user,
+        "pwd":password
       }
     }
-    header = {'content-type':'application/json'}
-    response = requests.post(
-      url,
-      data= json.dumps(payload),
-      headers= header,
-      verify= False
-    )
-    r_json = response.json()
-    token = r_json['imdata'][0]['aaaLogin']['attributes']['token']
-    return token
+  }
+  header = {'content-type':'application/json'}
+  response = requests.post(
+    url,
+    data= json.dumps(payload),
+    headers= header,
+    verify= False
+  )
+  r_json = response.json()
+  token = r_json['imdata'][0]['aaaLogin']['attributes']['token']
+  return {'APIC-Cookie':token}
+
+def gettenant(apic_ip, cookie):
+  url = apic_ip + "/api/node/class/fvTenant.json"
+  #header = {'content-type':'application-json', 'X-Auth-Token':cookie}
+  #header = {'content-type':'application-json', 'APIC-Cookie':token}
+  #payload = {}
+  response = requests.get(url, cookies=cookie, verify=False)
+  r_json = response.json()
+  return r_json
 
 thetoken = gettoken(controller, user, password)
-#print(thetoken)
+print(thetoken)
+tenants = gettenant(controller, thetoken)
+print(tenants)
